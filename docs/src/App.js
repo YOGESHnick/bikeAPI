791f6469b5f64acc736c2ca7ea44001840bb58ac
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2024-03-17 15:59:04 +0530

    built basic copying

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import "./App.css";
 
 function App() {
+  const [clipboardState, setClipboardState] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="container">
+        <CopyToClipboard
+          text="adangommala"
+          onCopy={() => setClipboardState(true)}
         >
-          Learn React
-        </a>
-      </header>
+          <button>
+          Click to copy
+            {clipboardState && (
+              <span style={{ color: "yellow" }}>
+                <br></br>copied
+              </span>
+            )}{" "}
+          </button>
+        </CopyToClipboard>
+
+        <h2>Paste it here</h2>
+        <textarea></textarea>
+      </div>
     </div>
   );
 }
