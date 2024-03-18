f079c5a2fdbc99489c12605437dab9bf60fecc35
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2024-03-18 10:25:22 +0530

    optimising

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Documentation from "./pages/Documentation";
 import Intro from "./pages/Intro";
-import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
